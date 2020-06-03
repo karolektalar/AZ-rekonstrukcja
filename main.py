91ddaf639415a27465a1a3bc24d7ffe5584c1eeb
Tomasz Fejcak <tfejcak@groupon.com>
2020-06-03 18:25:56 +0200

    dodana czesc algorytmu ktora zmienai pare wyszkuwiwanych wierzchołkow w przypadku kiedy nie sa poprawnym merge pointem

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ matrix_1 = np.array([
     [2, 2, 0, 2],
     [2, 2, 2, 0],
 ])
+# przyklad w ktorym pierwsza para jest zla
+matrix_2 = np.array([
+    [0, 4, 4, 5],
+    [4, 0, 6, 5],
+    [4, 6, 0, 7],
+    [5, 5, 7, 0]
+])
 
 
 def create_upper_triangular_matrix(matrix: np.array) -> np.array:
@@ -64,35 +71,42 @@ def check_if_distances_are_correct(matrix: np, a, b, distances_to_merge_vertex) 
 
 def make_graph_from_leaves(matrix):
     b = matrix
-    good_pair = False
+    bad_pair = False
 
     while b.shape[0] > 2:
 
-        upper_triangular_matrix = create_upper_triangular_matrix(b)
-        new_masked_matrix = np.ma.MaskedArray(upper_triangular_matrix, upper_triangular_matrix <= 0)
+        if not bad_pair:
+            upper_triangular_matrix = create_upper_triangular_matrix(b)
+            new_masked_matrix = np.ma.MaskedArray(upper_triangular_matrix, upper_triangular_matrix <= 0)
+
         smallest_distance_idx = find_smallest_distance_coordinates_in_matrix(new_masked_matrix)
-        distances_to_merge_vertex = create_distances_matrix_to_merge_vertex(new_masked_matrix, smallest_distance_idx[0],
+        distances_to_merge_vertex = create_distances_matrix_to_merge_vertex(upper_triangular_matrix, smallest_distance_idx[0],
                                                                             smallest_distance_idx[1])
 
-        if (check_if_distances_are_correct(new_masked_matrix, smallest_distance_idx[0], smallest_distance_idx[1],
+        if (check_if_distances_are_correct(upper_triangular_matrix, smallest_distance_idx[0], smallest_distance_idx[1],
                                            distances_to_merge_vertex)):
-            good_pair = True
             # Stworzenie macierzy B' w której X i Y sa usutenie a Z jest dodane
             b = np.hstack((new_masked_matrix, np.atleast_2d(distances_to_merge_vertex).T))
             b = np.vstack((b, [0] * b.shape[1]))
             b = np.delete(b, [smallest_distance_idx[0], smallest_distance_idx[1]], 0)
             b = np.delete(b, [smallest_distance_idx[0], smallest_distance_idx[1]], 1)
             print(b)
+            bad_pair = False
         else:
-            print("false")
-            good_pair = False
-
+            print(f"old masked = {new_masked_matrix}")
+            new_masked_matrix[smallest_distance_idx[0], smallest_distance_idx[1]] = np.ma.masked
+            print(f"new masked = {new_masked_matrix}")
+            print(new_masked_matrix.mask)
+            b = new_masked_matrix
+            bad_pair = True
+            if False not in new_masked_matrix.mask:
+                # in this step we can return -1, non any pair from input create valid pair
+                return -1
         # TODO : remove X and Y from B, add Z to B' --> DONE
-        # TODO : swap X and Y and repeat the steps if false
-        # TODO : if there is no more pairs to choose return -1
-        # TODO : repeat steps until there are two trees in S --> chyba DONE
+        # TODO : swap X and Y and repeat the steps if false --> DONE
+        # TODO : if there is no more pairs to choose return -1 -> to mozna zrobic jesli macierz is fully masked to -1 [Wydaje mi sie ze DONE]
+        # TODO : repeat steps until there are two trees in S --> wydaje mi sie ze DONE
 
-    if not good_pair:
-        return -1
+
 if __name__ == '__main__':
-    make_graph_from_leaves(matrix_1)
+    make_graph_from_leaves(matrix_2)
