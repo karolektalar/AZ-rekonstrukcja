8d2db11e4f7cb296e6517bb9698979672aa03da4
karol <ktalarek@ittouch.pl>
2020-06-04 22:42:04 +0200

    add merge trees (with bug)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 matrix_1 = np.array([
     [0, 2, 2, 2],
@@ -8,11 +9,30 @@ matrix_1 = np.array([
 ])
 # przyklad w ktorym pierwsza para jest zla
 matrix_2 = np.array([
-    [0, 4, 4, 5],
-    [4, 0, 6, 5],
-    [4, 6, 0, 7],
-    [5, 5, 7, 0]
+    # [0, 2, 3, 3],
+    # [2, 0, 3, 3],
+    # [3, 3, 0, 2],
+    # [3, 3, 2, 0]
+    # [0, 3, 2, 3, 5, 3],
+    # [3, 0, 3, 2, 4, 2],
+    # [2, 3, 0, 3, 5, 3],
+    # [3, 2, 3, 0, 4, 2],
+    # [5, 4, 5, 4, 0, 4],
+    # [3, 2, 3, 2, 4, 0]
+    [0, 3, 2, 3, 5, 3, 4, 5, 5, 3],
+    [3, 0, 3, 2, 4, 2, 3, 4, 4, 2],
+    [2, 3, 0, 3, 5, 3, 4, 5, 5, 3],
+    [3, 2, 3, 0, 4, 2, 3, 4, 4, 2],
+    [5, 4, 5, 4, 0, 4, 3, 2, 2, 4],
+    [3, 2, 3, 2, 4, 0, 3, 4, 4, 2],
+    [4, 3, 4, 3, 3, 3, 0, 3, 3, 3],
+    [5, 4, 5, 4, 2, 4, 3, 0, 2, 4],
+    [5, 4, 5, 4, 2, 4, 3, 2, 0, 4],
+    [3, 2, 3, 2, 4, 2, 3, 4, 4, 0]
+
+
 ])
+
 
 
 def create_upper_triangular_matrix(matrix: np.array) -> np.array:
@@ -68,30 +88,79 @@ def check_if_distances_are_correct(matrix: np, a, b, distances_to_merge_vertex) 
                     return False
     return True
 
+#TODO nie działa coś z mergeindex i się źle merguje :(
+def merge_subgraphs(new_subgraph, first_subgraph):
+    merged_subgraph = new_subgraph
+    add_edge = False
+    if not type(first_subgraph) == type(0):
+        new_subgraph_merge_idx = np.unravel_index(new_subgraph.argmax(), new_subgraph.shape)[0]
+        merge_idx = np.unravel_index(first_subgraph.argmax(), first_subgraph.shape)
+        if new_subgraph_merge_idx == len(new_subgraph[0])-1:
+            add_edge = True
+        first_subgraph[merge_idx] = 0
+        deleted_column = new_subgraph[:, len(new_subgraph[0])-1]
+        new_subgraph = np.delete(new_subgraph, len(new_subgraph[0])-1, 0)
+        new_subgraph = np.delete(new_subgraph, len(new_subgraph[0])-1, 1)
+        tmp = np.zeros((len(new_subgraph[0]), len(first_subgraph[0])))
+        tmp = np.concatenate((first_subgraph, tmp))
+        tmp2 = np.zeros((len(first_subgraph[0]), len(new_subgraph[0])))
+        tmp2 = np.concatenate((tmp2, new_subgraph))
+        tmp = np.concatenate((tmp, tmp2), axis=1)
+        for i in range(len(deleted_column)):
+            if (deleted_column[i] == 1):
+                tmp[int(merge_idx[0])][len(first_subgraph)+i] = 1
+        if add_edge:
+            tmp[merge_idx[0]][merge_idx[0]] = 2
+        merged_subgraph = tmp
+    return merged_subgraph
+
 
 def make_graph_from_leaves(matrix):
+    subgraphs_matrix = [0] * len(matrix[0])
     b = matrix
     bad_pair = False
 
-    while b.shape[0] > 2:
+    while b.shape[0] > 1:
 
         if not bad_pair:
             upper_triangular_matrix = create_upper_triangular_matrix(b)
             new_masked_matrix = np.ma.MaskedArray(upper_triangular_matrix, upper_triangular_matrix <= 0)
 
         smallest_distance_idx = find_smallest_distance_coordinates_in_matrix(new_masked_matrix)
+
         distances_to_merge_vertex = create_distances_matrix_to_merge_vertex(upper_triangular_matrix, smallest_distance_idx[0],
                                                                             smallest_distance_idx[1])
 
         if (check_if_distances_are_correct(upper_triangular_matrix, smallest_distance_idx[0], smallest_distance_idx[1],
                                            distances_to_merge_vertex)):
-            # Stworzenie macierzy B' w której X i Y sa usutenie a Z jest dodane
+            smallest_distance = int(new_masked_matrix[smallest_distance_idx[0]][smallest_distance_idx[1]])
+            distances_to_merge_vertex_difference = (distances_to_merge_vertex[smallest_distance_idx[0]] - distances_to_merge_vertex[smallest_distance_idx[1]] + smallest_distance)/2
+
+            new_subgraph = np.zeros((smallest_distance+1, smallest_distance+1))
+            first_subgraph = subgraphs_matrix[smallest_distance_idx[0]]
+            second_subgraph = subgraphs_matrix[smallest_distance_idx[1]]
+
+            for i in range(smallest_distance):
+                new_subgraph[i][i+1] = 1
+            new_subgraph[int(distances_to_merge_vertex_difference)][int(distances_to_merge_vertex_difference)] = 2
+            merged_subgraph = merge_subgraphs(new_subgraph, first_subgraph)
+            merged_subgraph = merge_subgraphs(merged_subgraph, second_subgraph)
+            # Stworzenie macierzy B' w której X i Y sa usunenie a Z jest dodane
+            new_masked_matrix.mask = np.ma.nomask
             b = np.hstack((new_masked_matrix, np.atleast_2d(distances_to_merge_vertex).T))
+            matrix = b.data
             b = np.vstack((b, [0] * b.shape[1]))
+            matrix = b.data
             b = np.delete(b, [smallest_distance_idx[0], smallest_distance_idx[1]], 0)
             b = np.delete(b, [smallest_distance_idx[0], smallest_distance_idx[1]], 1)
-            print(b)
+            b.mask = np.ma.nomask
+            subgraphs_matrix.pop(max(smallest_distance_idx[0], smallest_distance_idx[1]))
+            subgraphs_matrix.pop(min(smallest_distance_idx[0], smallest_distance_idx[1]))
+            subgraphs_matrix.append(merged_subgraph)
+            print("merged subgraphs")
+            print(merged_subgraph)
             bad_pair = False
+
         else:
             print(f"old masked = {new_masked_matrix}")
             new_masked_matrix[smallest_distance_idx[0], smallest_distance_idx[1]] = np.ma.masked
@@ -102,6 +171,8 @@ def make_graph_from_leaves(matrix):
             if False not in new_masked_matrix.mask:
                 # in this step we can return -1, non any pair from input create valid pair
                 return -1
+
+
         # TODO : remove X and Y from B, add Z to B' --> DONE
         # TODO : swap X and Y and repeat the steps if false --> DONE
         # TODO : if there is no more pairs to choose return -1 -> to mozna zrobic jesli macierz is fully masked to -1 [Wydaje mi sie ze DONE]
